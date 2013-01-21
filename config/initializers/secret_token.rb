34e2578864bd3fa6f6d898c816f4c0e290db3c42
everpointer <vane.zhangfeng@gmail.com>
2013-01-21 11:35:07 +0800

    This is first commit, done lq_movie basic construction

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LqMovie::Application.config.secret_token = 'e4759db9c4d51616282f79bbfc0f0665b12a280fd0170a7b76f443b93ecb57099fee3130a80cfb5a2d306ec353229514dd9e9e1e3563d90fd8f50e84eb556719'
